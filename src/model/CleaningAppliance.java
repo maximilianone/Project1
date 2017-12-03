59df4d615f7d247ad27da14d02d0eda39e6d89c4
maximilianone <maximilianone22@gmail.com>
2017-12-03 18:06:58 +0200

    Delete CleaningAppliance.java

--- src/model/CleaningAppliance.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class CleaningAppliance extends ElectricalAppliance {
-    public CleaningAppliance(String name, double size, double documentedPower){
-        super(name, "Cleaning Appliance", size, documentedPower);
-    }
-}
