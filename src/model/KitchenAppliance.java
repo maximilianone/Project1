ca8dd1f510a5d1980f41f35b49c7479a2a2c5389
maximilianone <maximilianone22@gmail.com>
2017-12-03 18:06:38 +0200

    Delete KitchenAppliance.java

--- src/model/KitchenAppliance.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public class KitchenAppliance extends ElectricalAppliance {
-    public KitchenAppliance(String name, double size, double documentedPower){
-        super(name, "Kitchen Appliance", size, documentedPower);
-    }
-
-}
