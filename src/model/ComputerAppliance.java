25ba14ae9781d945a61dfecb46e64f42c2e75436
maximilianone <maximilianone22@gmail.com>
2017-12-03 18:06:51 +0200

    Delete ComputerAppliance.java

--- src/model/ComputerAppliance.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class ComputerAppliance extends ElectricalAppliance{
-    public ComputerAppliance(String name, double size, double documentedPower){
-        super(name, "Computer Appliance", size, documentedPower);
-    }
-}
