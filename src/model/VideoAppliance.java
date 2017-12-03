6162ba4701a8b8f298fbcc9e7f8a32e819c3567c
maximilianone <maximilianone22@gmail.com>
2017-12-03 18:06:32 +0200

    Delete VideoAppliance.java

--- src/model/VideoAppliance.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class VideoAppliance extends ElectricalAppliance {
-    public VideoAppliance(String name, double size, double documentedPower){
-        super(name, "Video Appliance", size, documentedPower);
-    }
-}
